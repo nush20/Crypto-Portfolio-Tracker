6ae8009ed90f9cde44c20e4c049bdaed58897f5d
nush20 <anushka.nd02@gmail.com>
2025-07-05 11:49:54 +0530

    Initial Commit

---
+++ Crypto Portfolio Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto Portfolio Tracker
+//
+//  Created by Anushka on 05/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Portfolio Tracker/Crypto_Portfolio_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Portfolio_TrackerApp.swift
+//  Crypto Portfolio Tracker
+//
+//  Created by Anushka on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Portfolio_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
